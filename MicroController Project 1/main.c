7e07c188a039a55dfa993717792b164d795f5c6d
KieranLienau <kieran.lienau@draeger.com>
2020-10-19 19:08:33 +0200

    Initial Commit

---
+++ MicroController Project 1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  MicroController Project 1
+//
+//  Created by Kieran Lienau on 19.10.20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
