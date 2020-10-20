9e28bd366f1a2ad0c1edc3b4be18f00bb4754a20
Lukas <Lukas@DESKTOP-6FBB2TA>
2020-10-20 23:26:27 +0200

    Test3

--- MicroController Project 1/main.c
+++ MicroController Project 1/main.c
@@ -8,22 +8,24 @@
 #include <stdio.h>
 
 int main() {
-    
     double a, b, product;
         printf("Enter two numbers: ");
     scanf("%lf %lf", &a, &b);
-     
+
         // Calculating product
         product = a * b;
 
         // Result up to 2 decimal point is displayed using %.2lf
-    
+
         if ( product < 5 )
-        {printf("Zu klein. Wie dein Pimmel. Höhö. \n");
-        } else {
+        {
+        	printf("Zu klein. Wie dein Pimmel. Höhö. \n");
+        }
+        else {
             printf("Product = %lf \n", product);
         }
     
+
  
     printf("Na du Fickschnitzel");
     puts("Selber!");
