02f01a093e3738f1a2ad2a2e502fb22540483845
Lukas <Lukas@DESKTOP-6FBB2TA>
2020-10-20 23:28:07 +0200

    Mongo ist keine Frucht

--- MicroController Project 1/main.c
+++ MicroController Project 1/main.c
@@ -8,23 +8,8 @@
 #include <stdio.h>
 
 int main() {
-    
-    double a, b, product;
-        printf("Enter two numbers: ");
-    scanf("%lf %lf", &a, &b);
-     
-        // Calculating product
-        product = a * b;
 
-        // Result up to 2 decimal point is displayed using %.2lf
-    
-        if ( product < 5 )
-        {printf("Zu klein. Wie dein Pimmel. Höhö. \n");
-        } else {
-            printf("Product = %lf \n", product);
-        }
-    
- 
+ // hab da mal das irrelevante gelöscht du mongo!
     printf("Na du Fickschnitzel");
     puts("Selber!")
     
