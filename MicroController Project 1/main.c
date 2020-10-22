d5ac788a42e52b607d8076b36b7cedffda93ebe1
Lukas <Lukas@DESKTOP-6FBB2TA>
2020-10-22 17:02:08 +0200

    Mal ein Kartendeck als Test

--- MicroController Project 1/main.c
+++ MicroController Project 1/main.c
@@ -6,12 +6,35 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
 
  // hab da mal das irrelevante gelöscht du mongo!
-    printf("Na du Fickschnitzel");
-    puts("Selber!")
-    
+    printf("Na du Fickschnitzel\n");
+    puts("Selber!");
+
+	char Kartenwert[13] = {'2','3','4','5','6','7','8','9','Z','B','D','K','A'};
+	char Kartenfarbe[4] = {'C','H','P','K'};
+	char Deck[53][4] = { NULL };
+	int zaehler=0;
+	int i;
+	int j;
+	for (i=0;i < 13;i++){
+		for (j=0; j < 4;j++){
+		char karte[3] = {Kartenfarbe[j],Kartenwert[i]};
+		strcat(Deck[zaehler],karte);
+		zaehler++;
+		}
+	}
+
+
+	int k;
+	for (k=0;k<52;k++){
+
+		printf("Karte %i = %s\n",k+1 ,Deck + k);
+
+	}
         return 0;
 }
